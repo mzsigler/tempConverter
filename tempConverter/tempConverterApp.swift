0d6d798bd9b42671f07fc57451301cd3db9bbc09
Mikel Sigler <mzsigler@gmail.com>
2024-01-31 11:35:34 -0600

    Initial Commit

---
+++ tempConverter/tempConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  tempConverterApp.swift
+//  tempConverter
+//
+//  Created by Mikel Sigler on 1/31/24.
+//
+
+import SwiftUI
+
+@main
+struct tempConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
