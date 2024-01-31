cfc4dff998aea4a70a89a4f30b155214b3245b0b
Mikel Sigler <mzsigler@gmail.com>
2024-01-31 13:10:33 -0600

    Everything structured but my math doesn't work lmao

--- tempConverter/ContentView.swift
+++ tempConverter/ContentView.swift
@@ -9,25 +9,77 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var input = 0.0
-    @State private var inputScale = "Fahrenheit"
+    @State private var inputType = "Fahrenheit"
+    @State private var outputType = "Celcius"
+    
+    var output: Double {
+        
+        var baseInput = 0.0
+        var displayOutput = 0.0
+        
+        if inputType == "Kelvin" {
+             baseInput = input
+        }
+        
+        if inputType == "Fahrenheit" {
+             baseInput = (input - 32) * 5/9 / 273.15
+        }
+        
+        if inputType == "Celcius" {
+             baseInput = input + 273.15
+        }
+        
+        if outputType == "Kelvin" {
+            displayOutput = baseInput
+        }
+        
+        if outputType == "Celcius" {
+            displayOutput = baseInput - 273.15
+        }
+        
+        if outputType == "Fahrenheit" {
+            displayOutput = (baseInput - 273.15) * 1.8 + 32
+        }
+        
+        return displayOutput
+        
+    }
     
     
-    let scales = ["Fahrenheit", "Celcius", "Kelvin"]
+    let inputScales = ["Fahrenheit", "Celcius", "Kelvin"]
+    let outputScales = ["Fahrenheit", "Celcius", "Kelvin"]
+    
+    
     
     var body: some View {
         NavigationStack{
             Form{
-                Section("Input"){
-                    TextField("Input", value: $input, format: .number)
-                }
                                 
                 Section("Input Type") {
-                    Picker("Tip Percentage", selection: $inputScale){
-                        ForEach(scales, id: \.self) {
+                    Picker("Input", selection: $inputType){
+                        ForEach(inputScales, id: \.self) {
                             Text($0)
                         }
                     }.pickerStyle(.segmented)
                 }
+                
+                Section("Input"){
+                    TextField("Input", value: $input, format: .number)
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section("Output Type") {
+                    Picker("Output", selection: $outputType){
+                        ForEach(outputScales, id: \.self) {
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+                
+                Text("\(input.formatted()) in \(inputType) is \(output) in \(outputType)")
+                    
+
+
                 
             }
             .navigationTitle("Temp Converter")
